3514388435855ce0bc0bfd28bfb37dc69fa95928
Mukesh Kumar <mukeshkumarmalhi7731@gmail.com>
2023-03-12 19:11:33 +0500

    test completed with seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,7 +20,7 @@ class UserSeeder extends Seeder
             'email'      => 'test_user@gmail.com',
             'address'    => 'Arham heights, punjab colony, karachi',
             'dob'        => date("Y-m-d", strtotime('02/25/1994')),
-            'password'   => bcrypt('12345678'),
+            'password'   => bcrypt('Variable$1'),
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
         ]);
